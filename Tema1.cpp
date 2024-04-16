ecd830ac90c04950b6df16e7e1b2bc2b96b328df
Voicu Librimir <voicu.librimir@gmail.com>
2024-04-16 21:39:38 +0300

    Add project files.

---
+++ Tema1.cpp
@@ -0,0 +1,17 @@
+// Tema1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+int main()
+{
+	const int anulCurent = 2024;
+	int anulnasterii;
+	std::cout << "Introdu anul nasterii tale :" << std::endl;
+	std::cin >> anulnasterii;
+	std::cout << "Varsta mea : " << anulCurent - anulnasterii << std::endl;
+
+
+}
+
